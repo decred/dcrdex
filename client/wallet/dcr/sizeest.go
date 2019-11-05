e1a5e56945b91baff9dba41e2a8f397b100322e6
Donald Adu-Poku <donald.adu@gmail.com>
2019-11-05 19:19:09 +0000

    add tx size estimation helpers.

---
+++ client/wallet/dcr/sizeest.go
@@ -0,0 +1,263 @@
+// Copyright (c) 2016 The btcsuite developers
+// Copyright (c) 2016-2019 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package dcr
+
+import (
+	"github.com/decred/dcrd/txscript/v2"
+	"github.com/decred/dcrd/wire"
+)
+
+// Worst case script and input/output size estimates.
+const (
+	// RedeemP2PKSigScriptSize is the worst case (largest) serialize size
+	// of a transaction input script that redeems a compressed P2PK output.
+	// It is calculated as:
+	//
+	//   - OP_DATA_73
+	//   - 72 bytes DER signature + 1 byte sighash
+	RedeemP2PKSigScriptSize = 1 + 73
+
+	// RedeemP2PKHSigScriptSize is the worst case (largest) serialize size
+	// of a transaction input script that redeems a compressed P2PKH output.
+	// It is calculated as:
+	//
+	//   - OP_DATA_73
+	//   - 72 bytes DER signature + 1 byte sighash
+	//   - OP_DATA_33
+	//   - 33 bytes serialized compressed pubkey
+	RedeemP2PKHSigScriptSize = 1 + 73 + 1 + 33
+
+	// RedeemP2SHSigScriptSize is the worst case (largest) serialize size
+	// of a transaction input script that redeems a P2SH output.
+	// It is calculated as:
+	//
+	//  - OP_DATA_73
+	//  - 73-byte signature
+	//  - OP_DATA_35
+	//  - OP_DATA_33
+	//  - 33 bytes serialized compressed pubkey
+	//  - OP_CHECKSIG
+	RedeemP2SHSigScriptSize = 1 + 73 + 1 + 1 + 33 + 1
+
+	// RedeemP2PKHInputSize is the worst case (largest) serialize size of a
+	// transaction input redeeming a compressed P2PKH output.  It is
+	// calculated as:
+	//
+	//   - 32 bytes previous tx
+	//   - 4 bytes output index
+	//   - 1 byte tree
+	//   - 8 bytes amount
+	//   - 4 bytes block height
+	//   - 4 bytes block index
+	//   - 1 byte compact int encoding value 107
+	//   - 107 bytes signature script
+	//   - 4 bytes sequence
+	RedeemP2PKHInputSize = 32 + 4 + 1 + 8 + 4 + 4 + 1 + RedeemP2PKHSigScriptSize + 4
+
+	// P2PKHPkScriptSize is the size of a transaction output script that
+	// pays to a compressed pubkey hash.  It is calculated as:
+	//
+	//   - OP_DUP
+	//   - OP_HASH160
+	//   - OP_DATA_20
+	//   - 20 bytes pubkey hash
+	//   - OP_EQUALVERIFY
+	//   - OP_CHECKSIG
+	P2PKHPkScriptSize = 1 + 1 + 1 + 20 + 1 + 1
+
+	// P2SHPkScriptSize is the size of a transaction output script that
+	// pays to a script hash.  It is calculated as:
+	//
+	//   - OP_HASH160
+	//   - OP_DATA_20
+	//   - 20 bytes script hash
+	//   - OP_EQUAL
+	P2SHPkScriptSize = 1 + 1 + 20 + 1
+
+	// TicketCommitmentScriptSize is the size of a ticket purchase commitment
+	// script. It is calculated as:
+	//
+	//   - OP_RETURN
+	//   - OP_DATA_30
+	//   - 20 bytes P2SH/P2PKH
+	//   - 8 byte amount
+	//   - 2 byte fee range limits
+	TicketCommitmentScriptSize = 1 + 1 + 20 + 8 + 2
+
+	// P2PKHOutputSize is the serialize size of a transaction output with a
+	// P2PKH output script.  It is calculated as:
+	//
+	//   - 8 bytes output value
+	//   - 2 bytes version
+	//   - 1 byte compact int encoding value 25
+	//   - 25 bytes P2PKH output script
+	P2PKHOutputSize = 8 + 2 + 1 + 25
+
+	// redeemAtomicSwapSigScriptSize is the worst case (largest) serialize size
+	// of a transaction input script to redeem the atomic swap contract.  This
+	// does not include final push for the contract itself.
+	//
+	//   - OP_DATA_73
+	//   - 72 bytes DER signature + 1 byte sighash
+	//   - OP_DATA_33
+	//   - 33 bytes serialized compressed pubkey
+	//   - OP_DATA_32
+	//   - 32 bytes secret
+	//   - OP_TRUE
+	redeemAtomicSwapSigScriptSize = 1 + 73 + 1 + 33 + 1 + 32 + 1
+
+	// refundAtomicSwapSigScriptSize is the worst case (largest) serialize size
+	// of a transaction input script that refunds a P2SH atomic swap output.
+	// This does not include final push for the contract itself.
+	//
+	//   - OP_DATA_73
+	//   - 72 bytes DER signature + 1 byte sighash
+	//   - OP_DATA_33
+	//   - 33 bytes serialized compressed pubkey
+	//   - OP_FALSE
+	refundAtomicSwapSigScriptSize = 1 + 73 + 1 + 33 + 1
+)
+
+// EstimateSerializeSize returns a worst case serialize size estimate for a
+// signed transaction that spends a number of outputs and contains each
+// transaction output from txOuts. The estimated size is incremented for an
+// additional change output if changeScriptSize is greater than 0. Passing 0
+// does not add a change output.
+func EstimateSerializeSize(scriptSizes []int, txOuts []*wire.TxOut, changeScriptSize int) int {
+	// Generate and sum up the estimated sizes of the inputs.
+	txInsSize := 0
+	for _, size := range scriptSizes {
+		txInsSize += EstimateInputSize(size)
+	}
+
+	inputCount := len(scriptSizes)
+	outputCount := len(txOuts)
+	changeSize := 0
+	if changeScriptSize > 0 {
+		changeSize = EstimateOutputSize(changeScriptSize)
+		outputCount++
+	}
+
+	// 12 additional bytes are for version, locktime and expiry.
+	return 12 + (2 * wire.VarIntSerializeSize(uint64(inputCount))) +
+		wire.VarIntSerializeSize(uint64(outputCount)) +
+		txInsSize +
+		sumOutputSerializeSizes(txOuts) +
+		changeSize
+}
+
+// EstimateSerializeSizeFromScriptSizes returns a worst case serialize size
+// estimate for a signed transaction that spends len(inputSizes) previous
+// outputs and pays to len(outputSizes) outputs with scripts of the provided
+// worst-case sizes. The estimated size is incremented for an additional
+// change output if changeScriptSize is greater than 0. Passing 0 does not
+// add a change output.
+func EstimateSerializeSizeFromScriptSizes(inputSizes []int, outputSizes []int, changeScriptSize int) int {
+	// Generate and sum up the estimated sizes of the inputs.
+	txInsSize := 0
+	for _, inputSize := range inputSizes {
+		txInsSize += EstimateInputSize(inputSize)
+	}
+
+	// Generate and sum up the estimated sizes of the outputs.
+	txOutsSize := 0
+	for _, outputSize := range outputSizes {
+		txOutsSize += EstimateOutputSize(outputSize)
+	}
+
+	inputCount := len(inputSizes)
+	outputCount := len(outputSizes)
+	changeSize := 0
+	if changeScriptSize > 0 {
+		changeSize = EstimateOutputSize(changeScriptSize)
+		outputCount++
+	}
+
+	// 12 additional bytes are for version, locktime and expiry.
+	return 12 + (2 * wire.VarIntSerializeSize(uint64(inputCount))) +
+		wire.VarIntSerializeSize(uint64(outputCount)) +
+		txInsSize + txOutsSize + changeSize
+}
+
+// EstimateInputSize returns the worst case serialize size estimate for a tx input
+//   - 32 bytes previous tx
+//   - 4 bytes output index
+//   - 1 byte tree
+//   - 8 bytes amount
+//   - 4 bytes block height
+//   - 4 bytes block index
+//   - the compact int representation of the script size
+//   - the supplied script size
+//   - 4 bytes sequence
+func EstimateInputSize(scriptSize int) int {
+	return 32 + 4 + 1 + 8 + 4 + 4 + wire.VarIntSerializeSize(uint64(scriptSize)) + scriptSize + 4
+}
+
+// EstimateOutputSize returns the worst case serialize size estimate for a tx output
+//   - 8 bytes amount
+//   - 2 bytes version
+//   - the compact int representation of the script size
+//   - the supplied script size
+func EstimateOutputSize(scriptSize int) int {
+	return 8 + 2 + wire.VarIntSerializeSize(uint64(scriptSize)) + scriptSize
+}
+
+func sumOutputSerializeSizes(outputs []*wire.TxOut) (serializeSize int) {
+	for _, txOut := range outputs {
+		serializeSize += txOut.SerializeSize()
+	}
+	return serializeSize
+}
+
+// inputSize returns the size of the transaction input needed to include a
+// signature script with size sigScriptSize.  It is calculated as:
+//
+//   - 32 bytes previous tx
+//   - 4 bytes output index
+//   - 1 byte tree
+//   - 8 bytes amount
+//   - 4 bytes block height
+//   - 4 bytes block index
+//   - Compact int encoding sigScriptSize
+//   - sigScriptSize bytes signature script
+//   - 4 bytes sequence
+func inputSize(sigScriptSize int) int {
+	return 32 + 4 + 1 + 8 + 4 + 4 + wire.VarIntSerializeSize(uint64(sigScriptSize)) + sigScriptSize + 4
+}
+
+// estimateRedeemSerializeSize returns a worst case serialize size estimates for
+// a transaction that redeems an atomic swap P2SH output.
+func estimateRedeemSerializeSize(contract []byte, txOuts []*wire.TxOut) int {
+	contractPush, err := txscript.NewScriptBuilder().AddData(contract).Script()
+	if err != nil {
+		// Should never be hit since this script does exceed the limits.
+		panic(err)
+	}
+	contractPushSize := len(contractPush)
+
+	// 12 additional bytes are for version, locktime and expiry.
+	return 12 + (2 * wire.VarIntSerializeSize(1)) +
+		wire.VarIntSerializeSize(1) +
+		inputSize(redeemAtomicSwapSigScriptSize+contractPushSize) +
+		sumOutputSerializeSizes(txOuts)
+}
+
+// estimateRefundSerializeSize returns a worst case serialize size estimates for
+// a transaction that refunds an atomic swap P2SH output.
+func estimateRefundSerializeSize(contract []byte, txOuts []*wire.TxOut) int {
+	contractPush, err := txscript.NewScriptBuilder().AddData(contract).Script()
+	if err != nil {
+		// Should never be hit since this script does exceed the limits.
+		panic(err)
+	}
+	contractPushSize := len(contractPush)
+
+	// 12 additional bytes are for version, locktime and expiry.
+	return 12 + (2 * wire.VarIntSerializeSize(1)) +
+		wire.VarIntSerializeSize(1) +
+		inputSize(refundAtomicSwapSigScriptSize+contractPushSize) +
+		sumOutputSerializeSizes(txOuts)
+}
