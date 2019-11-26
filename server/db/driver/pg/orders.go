2f08c5faaf37f2762e35bf20b51ab920e776f974
David Hill <dhill@mindcry.org>
2019-11-26 13:53:20 -0500

    build: fix govet warnings

--- server/db/driver/pg/orders.go
+++ server/db/driver/pg/orders.go
@@ -87,14 +87,14 @@ type utxoSlice []order.Outpoint
 
 // Value implements the sql/driver.Valuer interface.
 func (us utxoSlice) Value() (driver.Value, error) {
-	return pq.GenericArray{us}.Value()
+	return pq.GenericArray{A: us}.Value()
 }
 
 // Scan implements the sql.Scanner interface.
 func (us *utxoSlice) Scan(src interface{}) error {
 	// type utxo implements Scanner, not order.Outpoint.
 	var utxos []utxo
-	ga := pq.GenericArray{&utxos}
+	ga := pq.GenericArray{A: &utxos}
 	err := ga.Scan(src)
 	if err != nil {
 		return err
