648e67d8fc496844cb26c70c924dcacdb5f5215d
Donald Adu-Poku <donald.adu@gmail.com>
2020-01-23 18:53:49 +0000

    multi: add run_tests.sh

---
+++ run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+# run tests
+env GORACE="halt_on_error=1" go test -short ./...
+
+# golangci-lint (github.com/golangci/golangci-lint) is used to run each
+# static checker.
+
+# check linters
+golangci-lint run --disable-all --deadline=10m \
+  --enable=goimports \
+  --enable=govet \
+  --enable=gosimple \
+  --enable=unconvert \
+  --enable=structcheck \
+  --enable=ineffassign
