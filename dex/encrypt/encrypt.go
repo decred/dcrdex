a22a11ad8a862d73295d5dfd5fe1cdb1ea80164f
buck54321 <buck54321@gmail.com>
2020-02-11 08:25:49 -0600

    encrypt: key derivation and encryption
    Add a key derivation and encryption scheme based on argon2id
    and xchacha20poly1305.

---
+++ dex/encrypt/encrypt.go
@@ -0,0 +1,245 @@
+// This code is available on the terms of the project LICENSE.md file,
+// also available online at https://blueoakcouncil.org/license/1.0.0.
+
+package encrypt
+
+import (
+	"crypto/rand"
+	"fmt"
+	"runtime"
+
+	"decred.org/dcrdex/dex/encode"
+	"golang.org/x/crypto/argon2"
+	"golang.org/x/crypto/chacha20poly1305"
+	"golang.org/x/crypto/poly1305"
+)
+
+// Crypter is an interface for an encryption key and encryption/decryption
+// algorithms. Create a Crypter with the NewCrypter function.
+type Crypter interface {
+	// Encrypt encrypts the plaintext.
+	Encrypt(b []byte) ([]byte, error)
+	// Decrypt decrypts the ciphertext created by Encrypt.
+	Decrypt(b []byte) ([]byte, error)
+	// Serialize serializes the Crypter. Use the Deserialize function to create
+	// a Crypter from the resulting bytes. Deserializing requires the password
+	// used to create the Crypter.
+	Serialize() []byte
+	// Close zeros the encryption key. The Crypter is useless after closing.
+	Close()
+}
+
+const (
+	// defaultTime is the default time parameter for argon2id key derivation.
+	defaultTime = 1
+	// defaultMem is the default memory parameter for argon2id key derivation.
+	defaultMem = 64 * 1024
+	// KeySize is the size of the encryption key.
+	KeySize = 32
+	// SaltSize is the size of the argon2id salt.
+	SaltSize = 16
+)
+
+// Big-endian encoder.
+var intCoder = encode.IntCoder
+
+// Key is 32 bytes.
+type Key [KeySize]byte
+
+// Salt is randomness used as part of key deriviation. This is different from
+// the salt generated during xchacha20poly1305 encryption, which is shorter.
+type Salt [SaltSize]byte
+
+// newSalt is the constructor for a salt based on randomness from crypto/rand.
+func newSalt() Salt {
+	var s Salt
+	_, err := rand.Read(s[:])
+	if err != nil {
+		panic("newSalt: " + err.Error())
+	}
+	return s
+}
+
+// argonParams is a set of parameters for key derivation.
+type argonParams struct {
+	time    uint32
+	memory  uint32
+	threads uint8
+}
+
+// NewCrypter derives an encryption key from a password string.
+func NewCrypter(pw string) Crypter {
+	return newArgonPolyCrypter(pw)
+}
+
+// Deserialize deserializes the Crypter for the password.
+func Deserialize(pw string, encCrypter []byte) (Crypter, error) {
+	ver, pushes, err := encode.DecodeBlob(encCrypter)
+	if err != nil {
+		return nil, err
+	}
+	switch ver {
+	case 0:
+		return decodeArgonPoly_v0(pw, pushes)
+	default:
+		return nil, fmt.Errorf("unknown Crypter version %d", ver)
+	}
+}
+
+// argonPolyCryper is an encryption algorithm based on argon2id for key
+// derivation and xchacha20poly1305 for symmetric encryption.
+type argonPolyCrypter struct {
+	key    Key
+	tag    [poly1305.TagSize]byte
+	salt   Salt
+	params *argonParams
+}
+
+// newArgonPolyCrypter is the constructor for an argonPolyCrypter.
+func newArgonPolyCrypter(pw string) *argonPolyCrypter {
+	pwB := []byte(pw)
+	salt := newSalt()
+	threads := uint8(runtime.NumCPU())
+
+	keyB := argon2.IDKey(pwB, salt[:], defaultTime, defaultMem, threads, KeySize*2)
+	// The argon2id key is split into two keys, The encryption key is the first 32
+	// bytes.
+	var encKey Key
+	copy(encKey[:], keyB[:KeySize])
+	// MAC key is the second 32 bytes.
+	var polyKey [KeySize]byte
+	copy(polyKey[:], keyB[KeySize:])
+
+	c := &argonPolyCrypter{
+		key:  encKey,
+		salt: salt,
+		params: &argonParams{
+			time:    defaultTime,
+			memory:  defaultMem,
+			threads: threads,
+		},
+	}
+	// Use the mac key and the serialized parameters to generate the
+	// authenticator.
+	poly1305.Sum(&c.tag, c.serializeParams(), &polyKey)
+	return c
+}
+
+// Encrypt encrypts the plaintext.
+func (c *argonPolyCrypter) Encrypt(plainText []byte) ([]byte, error) {
+	boxer, err := chacha20poly1305.NewX(c.key[:])
+	if err != nil {
+		return nil, fmt.Errorf("aead error: %v", err)
+	}
+	nonce := make([]byte, boxer.NonceSize())
+	_, err = rand.Read(nonce)
+	if err != nil {
+		return nil, fmt.Errorf("nonce generation error: %v", err)
+	}
+	cipherText := boxer.Seal(nil, nonce, plainText, nil)
+	return encode.BuildyBytes{0}.AddData(nonce).AddData(cipherText), nil
+}
+
+// Decrypt decrypts the ciphertext created by Encrypt.
+func (c *argonPolyCrypter) Decrypt(encrypted []byte) ([]byte, error) {
+	ver, pushes, err := encode.DecodeBlob(encrypted)
+	if err != nil {
+		return nil, fmt.Errorf("DecodeBlob: %v", err)
+	}
+	if ver != 0 {
+		return nil, fmt.Errorf("only version 0 encryptions are known. got version %d", ver)
+	}
+	if len(pushes) != 2 {
+		return nil, fmt.Errorf("expected 2 pushes. got %d", len(pushes))
+	}
+	boxer, err := chacha20poly1305.NewX(c.key[:])
+	if err != nil {
+		return nil, fmt.Errorf("aead error: %v", err)
+	}
+	nonce, cipherText := pushes[0], pushes[1]
+	if len(nonce) != boxer.NonceSize() {
+		return nil, fmt.Errorf("incompatible nonce length. expected %d, got %d", boxer.NonceSize(), len(nonce))
+	}
+	plainText, err := boxer.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		return nil, fmt.Errorf("aead.Open: %v", err)
+	}
+	return plainText, nil
+}
+
+// Serialize serializes the argonPolyCrypter.
+func (c *argonPolyCrypter) Serialize() []byte {
+	return c.serializeParams().AddData(c.tag[:])
+}
+
+// serializeParams serializes the argonPolyCrypter parameters, without the
+// poly1305 auth tag.
+func (c *argonPolyCrypter) serializeParams() encode.BuildyBytes {
+	return encode.BuildyBytes{0}.
+		AddData(c.salt[:]).
+		AddData(encode.Uint32Bytes(c.params.time)).
+		AddData(encode.Uint32Bytes(c.params.memory)).
+		AddData([]byte{c.params.threads})
+}
+
+// Close zeros the key. The argonPolyCrypter is useless after closing.
+func (c *argonPolyCrypter) Close() {
+	for i := range c.key {
+		c.key[i] = 0
+	}
+}
+
+// decodeArgonPoly_v0 decodes an argonPolyCrypter from the pushes extracted from
+// a version 0 blob.
+func decodeArgonPoly_v0(pw string, pushes [][]byte) (*argonPolyCrypter, error) {
+	if len(pushes) != 5 {
+		return nil, fmt.Errorf("decodeArgonPoly_v0 expected 5 pushes, but got %d", len(pushes))
+	}
+	saltB, tagB := pushes[0], pushes[4]
+	timeB, memB, threadsB := pushes[1], pushes[2], pushes[3]
+
+	if len(saltB) != SaltSize {
+		return nil, fmt.Errorf("expected salt of length %d, got %d", SaltSize, len(saltB))
+	}
+	var salt Salt
+	copy(salt[:], saltB)
+
+	if len(threadsB) != 1 {
+		return nil, fmt.Errorf("threads parameter encoding length is incorrect. expected 1, got %d", len(threadsB))
+	}
+
+	if len(tagB) != poly1305.TagSize {
+		return nil, fmt.Errorf("mac authenticator of incorrect length. wanted %d, got %d", poly1305.TagSize, len(tagB))
+	}
+	var polyTag [poly1305.TagSize]byte
+	copy(polyTag[:], tagB)
+
+	// Create the argonPolyCrypter.
+	params := &argonParams{
+		time:    intCoder.Uint32(timeB),
+		memory:  intCoder.Uint32(memB),
+		threads: threadsB[0],
+	}
+	c := &argonPolyCrypter{
+		salt:   salt,
+		tag:    polyTag,
+		params: params,
+	}
+
+	// Prepare the key.
+	pwB := []byte(pw)
+	keyB := argon2.IDKey(pwB, c.salt[:], params.time, params.memory, params.threads, KeySize*2)
+
+	// Check the poly1305 auth to verify the password.
+	polyKeyB := keyB[KeySize:]
+	var polyKey [KeySize]byte
+	copy(polyKey[:], polyKeyB)
+	if !poly1305.Verify(&c.tag, c.serializeParams(), &polyKey) {
+		return nil, fmt.Errorf("incorrect password")
+	}
+
+	// Copy the key.
+	copy(c.key[:], keyB[:KeySize])
+
+	return c, nil
+}
---
+++ dex/encrypt/encrypt_test.go
@@ -0,0 +1,182 @@
+// This code is available on the terms of the project LICENSE.md file,
+// also available online at https://blueoakcouncil.org/license/1.0.0.
+
+package encrypt
+
+import (
+	"bytes"
+	"math/rand"
+	"testing"
+
+	"decred.org/dcrdex/dex/encode"
+)
+
+var (
+	copyB = encode.CopySlice
+	randB = encode.RandomBytes
+)
+
+func TestDecrypt(t *testing.T) {
+	pw := "4kliaOCha2"
+	crypter := NewCrypter(pw)
+	thing := randB(50)
+	encThing, err := crypter.Encrypt(thing)
+	if err != nil {
+		t.Fatalf("Encrypt error: %v", err)
+	}
+
+	reCheck := func() {
+		reThing, err := crypter.Decrypt(encThing)
+		if err != nil {
+			t.Fatalf("Decrypt error: %v", err)
+		}
+		if !bytes.Equal(thing, reThing) {
+			t.Fatalf("%x != %x", thing, reThing)
+		}
+	}
+	reCheck()
+
+	// Change the version.
+	var badThing encode.BuildyBytes = copyB(encThing)
+	badThing[0] = 1
+	_, err = crypter.Decrypt(badThing)
+	if err == nil {
+		t.Fatalf("no error for wrong version")
+	}
+	reCheck()
+
+	// Drop the second push.
+	ver, pushes, _ := encode.DecodeBlob(encThing)
+	badThing = encode.BuildyBytes{ver}.AddData(pushes[0])
+	_, err = crypter.Decrypt(badThing)
+	if err == nil {
+		t.Fatalf("no error for trunacted pushes")
+	}
+	reCheck()
+
+	// Wrong size nonce.
+	badThing = encode.BuildyBytes{ver}.AddData(append(copyB(pushes[0]), 5)).AddData(pushes[1])
+	_, err = crypter.Decrypt(badThing)
+	if err == nil {
+		t.Fatalf("no error for extended nonce")
+	}
+	reCheck()
+
+	// Corrupted blob
+	badThing = append(copyB(encThing), 123)
+	_, err = crypter.Decrypt(badThing)
+	if err == nil {
+		t.Fatalf("no error for corrupted blob")
+	}
+	reCheck()
+
+}
+
+func TestSerialize(t *testing.T) {
+	pw := "20O6KcujCU"
+	crypter := NewCrypter(pw)
+	thing := randB(50)
+	encThing, err := crypter.Encrypt(thing)
+	if err != nil {
+		t.Fatalf("Encrypt error: %v", err)
+	}
+	serializedCrypter := crypter.Serialize()
+	reCheck := func(tag string) {
+		reCrypter, err := Deserialize(pw, serializedCrypter)
+		if err != nil {
+			t.Fatalf("%s: reCheck deserialization error: %v", tag, err)
+		}
+		reThing, err := reCrypter.Decrypt(encThing)
+		if err != nil {
+			t.Fatalf("%s: reCrypter failed to decrypt thing: %v", tag, err)
+		}
+		if !bytes.Equal(reThing, thing) {
+			t.Fatalf("%s: reCrypter's decoded thing is messed up: %x != %x", tag, reThing, thing)
+		}
+	}
+	reCheck("first")
+
+	// Can't deserialize with wrong password.
+	_, err = Deserialize("wrong password", serializedCrypter)
+	if err == nil {
+		t.Fatalf("no Deserialize error for wrong password")
+	}
+	reCheck("after wrong password")
+
+	// Adding a random bytes should fail at DecodeBlob
+	badCrypter := append(copyB(serializedCrypter), 5)
+	_, err = Deserialize(pw, badCrypter)
+	if err == nil {
+		t.Fatalf("no Deserialize error for corrupted blob")
+	}
+	reCheck("after corrupted blob")
+
+	// Version 1 not known.
+	badCrypter = copyB(serializedCrypter)
+	badCrypter[0] = 1
+	_, err = Deserialize(pw, badCrypter)
+	if err == nil {
+		t.Fatalf("no Deserialize error for blob from the future")
+	}
+	reCheck("after wrong password")
+
+	// Trim the salt, which is push 0 and normally 16 bytes.
+	badCrypter = trimPush(copyB(serializedCrypter), 0, 15)
+	_, err = Deserialize(pw, badCrypter)
+	if err == nil {
+		t.Fatalf("no Deserialize error trimmed salt")
+	}
+	reCheck("after trimmed salt")
+
+	// Trim the threads parameter, which is push 3 and normally 1 byte.
+	badCrypter = trimPush(copyB(serializedCrypter), 3, 0)
+	_, err = Deserialize(pw, badCrypter)
+	if err == nil {
+		t.Fatalf("no Deserialize error zero-length threads")
+	}
+	reCheck("after zero-length threads")
+
+	// Trim the tag parameter, which is push 4 and normally 16 bytes.
+	badCrypter = trimPush(copyB(serializedCrypter), 4, 15)
+	_, err = Deserialize(pw, badCrypter)
+	if err == nil {
+		t.Fatalf("no Deserialize error trimmed tag")
+	}
+	reCheck("after trimmed tag")
+}
+
+func TestRandomness(t *testing.T) {
+	pw := string(randB(15))
+	crypter := NewCrypter(pw)
+	numToDo := 10_000
+	if testing.Short() {
+		numToDo /= 10
+	}
+	for i := 0; i < numToDo; i++ {
+		thing := randB(rand.Intn(100))
+		encThing, err := crypter.Encrypt(thing)
+		if err != nil {
+			t.Fatalf("error encrypting %x with password %x", thing, []byte(pw))
+		}
+		reThing, err := crypter.Decrypt(encThing)
+		if err != nil {
+			t.Fatalf("error decrypting %x, which is encrypted %x with password %x", encThing, thing, []byte(pw))
+		}
+		if !bytes.Equal(thing, reThing) {
+			t.Fatalf("decrypted %x is different that encrypted %x with password %x", reThing, thing, []byte(pw))
+		}
+	}
+}
+
+func trimPush(b []byte, idx, newLen int) []byte {
+	ver, pushes, _ := encode.DecodeBlob(b)
+	newB := encode.BuildyBytes{ver}
+	for i := range pushes {
+		if i == idx {
+			newB = newB.AddData(pushes[i][:newLen])
+		} else {
+			newB = newB.AddData(pushes[i])
+		}
+	}
+	return newB
+}
