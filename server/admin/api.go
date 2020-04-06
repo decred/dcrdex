aac29c5bb9abb4472bc634fd4a702182a1b11a54
JoeGruff <joegruffins@gmail.com>
2020-04-07 08:43:54 +0900

    Fix grammar

--- server/admin/api.go
+++ server/admin/api.go
@@ -14,7 +14,7 @@ func writeJSON(w http.ResponseWriter, thing interface{}) {
 	writeJSONWithStatus(w, thing, http.StatusOK)
 }
 
-// writeJSON writes marshals the provided interface and writes the bytes to the
+// writeJSON marshals the provided interface and writes the bytes to the
 // ResponseWriter with the specified response code.
 func writeJSONWithStatus(w http.ResponseWriter, thing interface{}, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
