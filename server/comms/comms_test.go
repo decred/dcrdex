9ea282751f0b132b0eb40a4b35f2b73ba69d7976
Donald Adu-Poku <donald.adu@gmail.com>
2020-06-23 17:06:48 +0000

    comms: sleep a couple of pings.

--- server/comms/comms_test.go
+++ server/comms/comms_test.go
@@ -704,8 +704,8 @@ func TestOnline(t *testing.T) {
 		return err
 	}
 
-	// Sleep for a few  pongs to make sure the client doesn't disconnect.
-	time.Sleep(pongWait * 3)
+	// Sleep for a couple of pongs to make sure the client doesn't disconnect.
+	time.Sleep(pongWait * 2)
 
 	// Positive path.
 	err = sendToDEX("ok", "{}")
