6c6de74384e103db1c6527b48041e66a5182c9cf
Jonathan Chappelow <chappjc@protonmail.com>
2020-12-10 17:16:07 -0600

    Revert "dexcctl: Add rpcpass arg to simnet-setup script"
    This reverts commit c1cea6f6dea73144de81898a41fbd78609ae04f9.

--- client/cmd/dexcctl/simnet-setup.sh
+++ client/cmd/dexcctl/simnet-setup.sh
@@ -2,21 +2,17 @@
 # Set up DCR and BTC wallets and register with the DEX.
 # dcrdex, dexc, and the wallet simnet harnesses should all be running before
 # calling this script.
-
-RPC_PASS="${1:-abc}"
-
 set -e
-AUTH="./dexcctl -p abc -P ${RPC_PASS}"
 echo initializing
-${AUTH} init
+./dexcctl -p abc init
 echo configuring Decred wallet
-${AUTH} -p abc newwallet 42 ~/dextest/dcr/alpha/alpha.conf '{"account":"default"}'
+./dexcctl -p abc -p abc newwallet 42 ~/dextest/dcr/alpha/alpha.conf '{"account":"default"}'
 echo configuring Bitcoin wallet
-${AUTH} -p "" newwallet 0 ~/dextest/btc/alpha/alpha.conf '{"walletname":"gamma"}'
+./dexcctl -p abc -p "" newwallet 0 ~/dextest/btc/alpha/alpha.conf '{"walletname":"gamma"}'
 echo configuring Litecoin wallet
-${AUTH} -p "" newwallet 2 ~/dextest/ltc/alpha/alpha.conf '{"walletname":"gamma"}'
+./dexcctl -p abc -p "" newwallet 2 ~/dextest/ltc/alpha/alpha.conf '{"walletname":"gamma"}'
 echo registering with DEX
-${AUTH} register 127.0.0.1:17273 100000000 ~/dextest/dcrdex/rpc.cert
+./dexcctl -p abc register 127.0.0.1:17273 100000000 ~/dextest/dcrdex/rpc.cert
 echo mining fee confirmation blocks
 tmux send-keys -t dcr-harness:0 "./mine-alpha 1" C-m
 sleep 2
