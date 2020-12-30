67a53f7e3450bcf3ac9a41332f3f2f6c3bcdd915
amass <amassarwi@gmail.com>
2020-12-30 04:50:20 +0200

    write dbWallet to db before walletConnect as it might generate new address and right it as well

--- client/core/core.go
+++ client/core/core.go
@@ -1557,6 +1557,11 @@ func (c *Core) ReconfigureWallet(appPW []byte, assetID uint32, cfg map[string]st
 	if err != nil {
 		return newError(walletErr, "error loading wallet for %d -> %s: %v", assetID, unbip(assetID), err)
 	}
+	err = c.db.UpdateWallet(dbWallet)
+	if err != nil {
+		wallet.Disconnect()
+		return newError(dbErr, "error saving wallet configuration: %v", err)
+	}
 
 	// Must connect to ensure settings are good.
 	err = c.connectWallet(wallet)
@@ -1569,11 +1574,6 @@ func (c *Core) ReconfigureWallet(appPW []byte, assetID uint32, cfg map[string]st
 			wallet.Disconnect()
 			return newError(walletAuthErr, "wallet successfully connected, but errored unlocking. reconfiguration not saved: %v", err)
 		}
-	}
-	err = c.db.UpdateWallet(dbWallet)
-	if err != nil {
-		wallet.Disconnect()
-		return newError(dbErr, "error saving wallet configuration: %v", err)
 	}
 
 	c.connMtx.RLock()
