48afa128f5b8a56fcb08777263a3d19aeece4e91
amass <amassarwi@gmail.com>
2021-01-07 16:09:31 +0200

    formatting

--- client/rpcserver/handlers.go
+++ client/rpcserver/handlers.go
@@ -826,8 +826,8 @@ Registration is complete after the fee transaction has been confirmed.`,
     sell (bool): Whether the order is selling.
     base (int): The BIP-44 coin index for the market's base asset.
     quote (int): The BIP-44 coin index for the market's quote asset.
-    qty (int): The number of units to buy/sell. Must be a multiple of the lot siz          rate (int): The atoms quote asset to pay/accept per unit base asset. e.g.
-      156000 satoshi/DCR for the DCR(base)_BTC(quote).
+    qty (int): The number of units to buy/sell. Must be a multiple of the lot size.
+    rate (int): The atoms quote asset to pay/accept per unit base asset. e.g.
     immediate (bool): Require immediate match. Do not book the order.`,
 		returns: `Returns:
     obj: The order details.
@@ -946,15 +946,15 @@ Registration is complete after the fee transaction has been confirmed.`,
       "cancelling" (bool): Whether this order is in the process of cancelling.
       "canceled" (bool): Whether this order has been canceled.
       "tif" (string): "immediate" if this limit order will only match for one epoch.
-        "standing" if the order can continue matching until filled or cancelled.
-			"matches": (array): An array of matches associated with the order.
-			[
+      "standing" if the order can continue matching until filled or cancelled.
+      "matches": (array): An array of matches associated with the order.
+      [
         {
           "matchID (string): The match's ID."
           "status" (string): The match's status."
           "revoked" (bool): Indicates if match was revoked.
           "rate"    (int): The match's rate.
-					"qty"     (int): The match's amount.
+          "qty"     (int): The match's amount.
           "side"    (string): The match's side, "maker" or "taker".
           "feerate" (int): The match's fee rate.
           "swap"    (string): The match's swap transaction.
