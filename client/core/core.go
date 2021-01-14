54077f2ac4c74b6576045298cd3dd0af3731aee5
kevin <kjwilde@hotmail.com>
2021-01-13 18:06:33 -0600

    Use acct as account variable name

--- client/core/core.go
+++ client/core/core.go
@@ -2704,7 +2704,7 @@ func (c *Core) AccountExport(pw []byte, host string) (*Account, error) {
 	privKey := hex.EncodeToString(dc.acct.privKey.Serialize())
 	dc.acct.keyMtx.RUnlock()
 
-	account := &Account{
+	acct := &Account{
 		Host:      host,
 		AccountID: accountId,
 		PrivKey:   privKey,
@@ -2713,25 +2713,25 @@ func (c *Core) AccountExport(pw []byte, host string) (*Account, error) {
 		FeeCoin:   hex.EncodeToString(dc.acct.feeCoin),
 		IsPaid:    dc.acct.feePaid(),
 	}
-	return account, nil
+	return acct, nil
 }
 
 // AccountImport is used import an existing account into the db.
-func (c *Core) AccountImport(pw []byte, account Account) error {
+func (c *Core) AccountImport(pw []byte, acct Account) error {
 	crypter, err := c.encryptionKey(pw)
 	if err != nil {
 		return codedError(passwordErr, err)
 	}
 
-	_, err = addrHost(account.Host)
+	_, err = addrHost(acct.Host)
 	if err != nil {
 		return newError(addressParseErr, "error parsing address: %v", err)
 	}
 
 	var accountInfo db.AccountInfo
-	accountInfo.Host = account.Host
+	accountInfo.Host = acct.Host
 
-	pubKey, err := hex.DecodeString(account.PubKey)
+	pubKey, err := hex.DecodeString(acct.PubKey)
 	if err != nil {
 		return codedError(decodeErr, err)
 	}
@@ -2740,23 +2740,23 @@ func (c *Core) AccountImport(pw []byte, account Account) error {
 		return codedError(parseKeyErr, err)
 	}
 
-	accountInfo.Cert, err = hex.DecodeString(account.Cert)
+	accountInfo.Cert, err = hex.DecodeString(acct.Cert)
 	if err != nil {
 		return codedError(decodeErr, err)
 	}
 
-	accountInfo.FeeCoin, err = hex.DecodeString(account.FeeCoin)
+	accountInfo.FeeCoin, err = hex.DecodeString(acct.FeeCoin)
 	if err != nil {
 		return codedError(decodeErr, err)
 	}
 
 	if !c.verifyAccount(&accountInfo) {
-		return newError(accountVerificationErr, "Account not verified for host: "+account.Host, err)
+		return newError(accountVerificationErr, "Account not verified for host: "+acct.Host, err)
 	}
-	// verifyAccount populates c.conns, now we can access c.conns account data.
-	accountInfo.Paid = c.conns[account.Host].acct.isPaid
+	// verifyAccount populates c.conns, now we can access c.conns acct data.
+	accountInfo.Paid = c.conns[acct.Host].acct.isPaid
 
-	accountInfo.EncKey, err = crypter.Encrypt(c.conns[account.Host].acct.privKey.Serialize())
+	accountInfo.EncKey, err = crypter.Encrypt(c.conns[acct.Host].acct.privKey.Serialize())
 	if err != nil {
 		return codedError(decodeErr, err)
 	}
