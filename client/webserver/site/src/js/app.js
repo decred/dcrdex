37ca8f9ab971adf3f97693e4b35660864a44c023
amass <amassarwi@gmail.com>
2021-01-18 23:15:55 +0200

    typo

--- client/webserver/site/src/js/app.js
+++ client/webserver/site/src/js/app.js
@@ -479,7 +479,7 @@ export default class Application {
           return false
         }
         // If the notification order already exists we update it.
-        // In case market's orders list is empty or the notifaction order isn't
+        // In case market's orders list is empty or the notification order isn't
         // part of it we add it manually as this means the order was
         // just placed.
         if (!mkt.orders) mkt.orders = [order]
