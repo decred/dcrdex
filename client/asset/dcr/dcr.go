38baedc3b5bd38be6cfb8af196934493668a0cb7
Jonathan Chappelow <chappjc@protonmail.com>
2021-07-06 12:13:31 -0500

    client/asset/dcr: shutdown ws client on Connect error
    This shutsdown the websocket connection if there is an error
    in client setup after establishing the connection.
    
    With rpcclient/v6, there is a bug that requires skipping
    WaitForShutdown, which would hang.

--- client/asset/dcr/dcr.go
+++ client/asset/dcr/dcr.go
@@ -588,6 +588,21 @@ func (dcr *ExchangeWallet) Connect(ctx context.Context) (*sync.WaitGroup, error)
 		return nil, fmt.Errorf("Decred Wallet connect error: %w", err)
 	}
 
+	// The websocket client is connected now, so if any of the following checks
+	// fails and we return with a non-nil error, we must shutdown the rpc client
+	// or subsequent reconnect attempts will be met with "websocket client has
+	// already connected".
+	var success bool
+	defer func() {
+		if !success {
+			dcr.client.Shutdown()
+			// NOTE: WaitForShutdown hangs because of an rpcclient bug, so just
+			// Shutdown to close the websocket. rpcclient.Connect leaks a
+			// goroutine until the context is cancelled, which is acceptable.
+			// See dcrd@c9521b468f957479cd0802b1f5c250e9ecc1b947
+		}
+	}()
+
 	// Check the required API versions.
 	versions, err := dcr.client.Version(ctx)
 	if err != nil {
@@ -629,6 +644,7 @@ func (dcr *ExchangeWallet) Connect(ctx context.Context) (*sync.WaitGroup, error)
 
 	dcr.log.Infof("Connected to dcrwallet (JSON-RPC API v%s) proxying dcrd (JSON-RPC API v%s) on %v",
 		walletSemver, nodeSemver, curnet)
+	success = true
 
 	var wg sync.WaitGroup
 	wg.Add(1)
