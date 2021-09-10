2eaa733624d41414d96cb13e377be38af3cf340f
Jonathan Chappelow <chappjc@protonmail.com>
2021-09-10 15:28:18 -0500

    dex/networks/btc: recognize rpcconnect
    The bitcoin-cli setting, rpcconnect=<IP>, should override any host from
    rpcbind or the default.
    
    checkConfig is already overwriting RPCBind with the selected host and
    port from a combination of defaults and RPCPort, and now RPCConnect,
    so also patch RPCPort if the used port is taken from RPCBind.
    This consistent approach is now documented on the Config struct.

--- dex/networks/btc/config.go
+++ dex/networks/btc/config.go
@@ -33,15 +33,24 @@ const (
 )
 
 // Config holds the parameters needed to initialize an RPC connection to a btc
-// wallet or backend. Default values are used for RPCBind and/or RPCPort if not
-// set.
+// wallet or backend. When constructed with LoadConfigFromPath or
+// LoadConfigFromSettings from a settings map, the following is true:
+//  - Default values are used for RPCBind and/or RPCPort if not set.
+//  - RPCPort is ignored if RPCBind already includes a port, otherwise if set,
+//    RPCPort is joined with the host in RPCBind.
+//  - If set, RPCConnect will be reflected in RPCBind, overriding any host
+//    originally from that setting.
+// In short, RPCBind will contain both host and port that may or may not be
+// specified in RPCConnect and RPCPort.
 type Config struct {
-	RPCUser string `ini:"rpcuser"`
-	RPCPass string `ini:"rpcpassword"`
-	RPCBind string `ini:"rpcbind"`
-	RPCPort int    `ini:"rpcport"`
+	RPCUser    string `ini:"rpcuser"`
+	RPCPass    string `ini:"rpcpassword"`
+	RPCBind    string `ini:"rpcbind"`
+	RPCPort    int    `ini:"rpcport"`    // reflected in RPCBind unless it already included a port
+	RPCConnect string `ini:"rpcconnect"` // (bitcoin-cli) if set, reflected in RPCBind
 
-	// Below fields are only used on the client.
+	// Below fields are only used on the client. They would not be in the
+	// bitcoin.conf file.
 
 	UseSplitTx       bool    `ini:"txsplit"`
 	FallbackFeeRate  float64 `ini:"fallbackfee"`
@@ -93,7 +102,7 @@ func checkConfig(cfg *Config, name string, network dex.Network, ports NetPorts) 
 		port = strconv.Itoa(cfg.RPCPort)
 	}
 
-	// if RPCBind includes a port, it takes precedence over RPCPort
+	// If RPCBind includes a port, it takes precedence over RPCPort.
 	if cfg.RPCBind != "" {
 		h, p, err := net.SplitHostPort(cfg.RPCBind)
 		if err != nil {
@@ -105,8 +114,21 @@ func checkConfig(cfg *Config, name string, network dex.Network, ports NetPorts) 
 			}
 			if p != "" {
 				port = p
+				// Patch cfg.RPCPort for consistency with RPCBind.
+				if rpcPort, err := strconv.Atoi(port); err != nil {
+					// port was already parsed as an int by SplitHostPort, but
+					// be cautious.
+					cfg.RPCPort = rpcPort
+				}
 			}
 		}
+	}
+
+	// If RPCConnect is set, that's how the user has bitcoin-cli configured, so
+	// use that instead of rpcbind's host or default (localhost).
+	if cfg.RPCConnect != "" {
+		host = cfg.RPCConnect
+		// RPCConnect does not include port, so use what we got above.
 	}
 
 	// overwrite rpcbind to use for rpcclient connection
