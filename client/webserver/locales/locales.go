5abdffdc571cf46994a03033c2db0d5d5f5e48c3
Dominic Ting <DominicTing@users.noreply.github.com>
2021-09-11 22:16:24 -0500

    client: zh-CN translation
    This adds Simplified Chinese (PRC) translations, tag zh-CH.
    
    Co-authored-by: chappjc <chappjc@protonmail.com>

--- client/webserver/locales/locales.go
+++ client/webserver/locales/locales.go
@@ -8,5 +8,6 @@ func init() {
 	Locales = map[string]map[string]string{
 		"en-US": EnUS,
 		"pt-BR": PtBr,
+		"zh-CN": ZhCN,
 	}
 }
--- client/webserver/site/src/js/locales.js
+++ client/webserver/site/src/js/locales.js
@@ -96,7 +96,7 @@ export const ptBR = {
   [ID_NOWALLET]: 'sem carteira',
   [ID_WALLET_SYNC_PROGRESS]: 'carteira está {{ syncProgress }}% sincronizada',
   [ID_HIDE_ADDITIONAL_SETTINGS]: 'esconder configurações adicionais',
-  [ID_SHOW_ADDIIONAL_SETTINGS]: 'mostrar configurações adicionais',
+  [ID_SHOW_ADDITIONAL_SETTINGS]: 'mostrar configurações adicionais',
   [ID_BUY]: 'Comprar',
   [ID_SELL]: 'Vender',
   [ID_NOT_SUPPORTED]: '{{ asset }} não tem suporte',
@@ -128,9 +128,54 @@ export const ptBR = {
   [ID_API_ERROR]: 'Erro de API'
 }
 
+export const zhCN = {
+  [ID_NO_PASS_ERROR_MSG]: '密码不能为空',
+  [ID_NO_APP_PASS_ERROR_MSG]: '应用密码不能为空',
+  [ID_PASSWORD_NOT_MATCH]: '密码不相同',
+  [ID_SET_BUTTON_BUY]: '来自{{ asset }}的买入订单',
+  [ID_SET_BUTTON_SELL]: '来自{{ asset }}的卖出订单',
+  [ID_OFF]: '关闭',
+  [ID_READY]: '准备就绪', //  alt. 准备好
+  [ID_LOCKED]: '锁定',
+  [ID_NOWALLET]: '未连接钱包', // alt. 没有钱包
+  [ID_WALLET_SYNC_PROGRESS]: '钱包同步进度{{ syncProgress }}%',
+  [ID_HIDE_ADDITIONAL_SETTINGS]: '隐藏其它设置',
+  [ID_SHOW_ADDITIONAL_SETTINGS]: '显示其它设置',
+  [ID_BUY]: '买',
+  [ID_SELL]: '卖',
+  [ID_NOT_SUPPORTED]: '{{ asset }}不受支持',
+  [ID_CONNECTION_FAILED]: '连接到服务器 dex 失败。您可以关闭 dexc 并稍后重试或等待尝试重新连接。',
+  [ID_ORDER_PREVIEW]: '总计： {{ total }} {{ asset }}',
+  [ID_CALCULATING]: '计算中...',
+  [ID_ESTIMATE_UNAVAILABLE]: '估计不可用',
+  [ID_NO_ZERO_RATE]: '汇率不能为零',
+  [ID_NO_ZERO_QUANTITY]: '数量不能为零',
+  [ID_TRADE]: '交易',
+  [ID_NO_ASSET_WALLET]: '没有钱包 {{ asset }}',
+  [ID_EXECUTED]: '执行',
+  [ID_BOOKED]: '保留',
+  [ID_CANCELING]: '取消',
+  [ID_ACCT_UNDEFINED]: '帐户未定义。',
+  [ID_KEEP_WALLET_PASS]: '保留钱包密码',
+  [ID_NEW_WALLET_PASS]: '设置新的钱包密码',
+  [ID_LOT]: '批处理',
+  [ID_LOTS]: '批', // alt. 很多
+  [ID_UNKNOWN]: 'unknown', // TODO
+  [ID_EPOCH]: '时间',
+  [ID_SETTLING]: 'settling', // TODO - "settling" shows in the Your Orders table when the order is doing an atomic swap with another trade
+  [ID_NO_MATCH]: 'no match', // TODO - "no match" shows in the Your Orders table when the order did not match with another trade and is done
+  [ID_CANCELED]: 'canceled', // TODO - "canceled" shows in the Your Orders table when the order has been canceled
+  [ID_REVOKED]: 'revoked', // TODO - "revoked" shows in the Your Orders table when the order has failed during swap
+  [ID_WAITING_FOR_CONFS]: 'Waiting for confirmations...', // TODO - shows when the registration fee transaction is waiting to be mined (needs confirmations)
+  [ID_NONE_SELECTED]: 'none selected', // TODO - looks unused, but this indicates nothing is selected in some list
+  [ID_REGISTRATION_FEE_SUCCESS]: 'Registration fee payment successful!', // TODO - When the registration fee transaction reaches the required number of confirmations, this is shown
+  [ID_API_ERROR]: '接口错误'
+}
+
 const localesMap = {
   'en-us': enUS,
-  'pt-br': ptBR
+  'pt-br': ptBR,
+  'zh-cn': zhCN
 }
 
 /* locale will hold the locale loaded via setLocale. */
