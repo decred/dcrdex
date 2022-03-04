9546aa5f59bab47403133c7c5b6205de7fd8f382
Jonathan Chappelow <chappjc@protonmail.com>
2022-03-04 00:03:09 -0600

    v0.4.2

--- client/cmd/dexcctl/version.go
+++ client/cmd/dexcctl/version.go
@@ -25,7 +25,7 @@ const (
 	appName  string = "dexcctl"
 	appMajor uint   = 0
 	appMinor uint   = 4
-	appPatch uint   = 1
+	appPatch uint   = 2
 )
 
 // go build -v -ldflags "-X decred.org/dcrdex/client/cmd/dexcctl/main.appPreRelease= -X decred.org/dcrdex/client/cmd/dexcctl/main.appBuild=$(git rev-parse --short HEAD)"
