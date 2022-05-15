decred/dcrdex

commit e8fd8acab23fbb253b649a66a95a5a63d4be6b75

dex/runner: Connect error means On should be false

dex/runner.go
@@ -105,6 +105,9 @@ func (c *ConnectionMaster) Connect(ctx context.Context) (err error) {
 	c.mtx.Lock()
 	c.wg, err = c.connector.Connect(c.ctx)
 	c.mtx.Unlock()
+	if err != nil {
+		c.cancel() // otherwise On() says true
+	}
 	return err
 }
 

commit 7cedcf9890e74778415aaa43cebc0892fa6c25be

dex,client: restore dex  reconnect loop
This reverts e8fd8acab23fbb253b649a66a95a5a63d4be6b75, which broke the
reconnect loop that the dex.Connector implemented by
client/comms.(*wsConn) starts.

This also adds a call to Disconnect when the connector for a client
wallet returns a non-nil error so that the ConnectionMaster's On method
does not say true, which is what e8fd8ac had aimed to resolve.

client/core/wallet.go
@@ -233,8 +233,10 @@ func (w *xcWallet) connected() bool {
 // flag to true, and validates the deposit address. Use Disconnect to cleanly
 // shutdown the wallet.
 func (w *xcWallet) Connect() error {
-	// No parent context; use Disconnect instead.
-	err := w.connector.Connect(context.Background())
+	// No parent context; use Disconnect instead. Also note that there's no
+	// reconnect loop for wallet like with the server Connectors, so we use
+	// ConnectOnce so that the ConnectionMaster's On method will report false.
+	err := w.connector.ConnectOnce(context.Background())
 	if err != nil {
 		return err
 	}

client/websocket/websocket.go
@@ -138,10 +138,10 @@ func (s *Server) connect(ctx context.Context, conn ws.Connection, addr string) {
 	// sending before it is connected.
 	s.clientsMtx.Lock()
 	cm := dex.NewConnectionMaster(cl)
-	err := cm.Connect(ctx)
+	err := cm.ConnectOnce(ctx) // we discard the cm anyway, but good practice
 	if err != nil {
 		s.clientsMtx.Unlock()
-		s.log.Errorf("websocketHandler client Connect: %v")
+		s.log.Errorf("websocketHandler client connect: %v", err)
 		return
 	}
 

dex/runner.go
@@ -99,14 +99,31 @@ func NewConnectionMaster(c Connector) *ConnectionMaster {
 }
 
 // Connect connects the Connector, and returns any initial connection error. Use
-// Disconnect to shut down the Connector.
+// Disconnect to shut down the Connector. Even if Connect returns a non-nil
+// error, On will report true until Disconnect is called. You would use Connect
+// if the wrapped Connector has a reconnect loop to continually attempt to
+// establish a connection even if the initial attempt fails. Use ConnectOnce if
+// the Connector should be given one chance to connect before being considered
+// not to be "on". If the ConnectionMaster is discarded on error, it is not
+// important which method is used.
 func (c *ConnectionMaster) Connect(ctx context.Context) (err error) {
 	c.init(ctx)
 	c.mtx.Lock()
 	c.wg, err = c.connector.Connect(c.ctx)
 	c.mtx.Unlock()
-	if err != nil {
-		c.cancel() // otherwise On() says true
+	// NOTE: Even if err is non-nil, we can't cancel the internal context
+	// because the connector may be attempting to reconnect. The caller should
+	// decide to let it go or to call Disconnect.
+	return err
+}
+
+// ConnectOnce is like Connect, but on error the internal status is updated so
+// that the On method returns false. This method may be used if an error from
+// the Connector is terminal. The caller may also use Connect if they cancel the
+// parent context or call Disconnect.
+func (c *ConnectionMaster) ConnectOnce(ctx context.Context) (err error) {
+	if err = c.Connect(ctx); err != nil {
+		c.cancel()
 	}
 	return err
 }

server/comms/server.go
@@ -713,11 +713,14 @@ func (s *Server) disconnectClients() {
 func (s *Server) addClient(ctx context.Context, client *wsLink) (*dex.ConnectionMaster, error) {
 	s.clientMtx.Lock()
 	defer s.clientMtx.Unlock()
+	cm := dex.NewConnectionMaster(client)
+	if err := cm.ConnectOnce(ctx); err != nil {
+		return nil, err
+	}
 	client.id = s.counter
 	s.counter++
 	s.clients[client.id] = client
-	cm := dex.NewConnectionMaster(client)
-	return cm, cm.Connect(ctx)
+	return cm, nil
 }
 
 // Remove the client from the map.