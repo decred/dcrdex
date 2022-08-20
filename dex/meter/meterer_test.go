13ac333ff4f82ed7617ade81e12271072b50ec28
yury <blablablabla@gmail.com>
2022-08-20 14:11:26 +0300

    Redesing test to avoid having to deal with Go scheduler

--- dex/meter/meterer_test.go
+++ dex/meter/meterer_test.go
@@ -10,33 +10,22 @@ import (
 func TestDelayedRelay(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	minDelay := time.Millisecond * 20
-	// While `DelayedRelay` sends signals at least `minDelay` apart, this test
-	// actually checks not the distance in time between those sends, but rather
-	// the distance in time between receives corresponding to those sends, and
-	// that distance can be less than `minDelay` (it also depends on Go scheduler),
-	// hence we lower our expectations here to account for possible Go scheduler
-	// lag.
-	minDelaySafe := minDelay - time.Millisecond*10
 	out, in := DelayedRelay(ctx, minDelay, 4)
 	defer func() { <-out }() // wait for channel close (relay shutdown)
 	defer cancel()
 
-	times := make([]time.Time, 0)
+	gotTimes := make([]time.Time, 0)
 
 	countSignals := func() (n int) {
 		for {
 			select {
 			case <-out:
-				times = append(times, time.Now())
+				gotTimes = append(gotTimes, time.Now())
 				n++
 			case <-time.After(minDelay * 2):
 				return
 			}
 		}
-	}
-
-	lastDelay := func() time.Duration {
-		return times[len(times)-1].Sub(times[len(times)-2])
 	}
 
 	sendSignals := func(n int, err error) {
@@ -50,23 +39,25 @@ func TestDelayedRelay(t *testing.T) {
 		t.Fatalf("wrong signal count. expected 1, got %d", n)
 	}
 
-	// Sending two signals simultaneously should end up with two signals, at
-	// least minDelaySafe apart.
+	// 2nd signal should be delayed at least for minDelay.
+	sentTime := time.Now()
 	go sendSignals(2, nil)
 	if n := countSignals(); n != 2 {
 		t.Fatalf("wrong signal count. expected 2, got %d", n)
 	}
-	if d := lastDelay(); d < minDelaySafe {
-		t.Fatalf("last delay was less than minimum. %s < %s", d, minDelaySafe)
+	if d := gotTimes[len(gotTimes)-1].Sub(sentTime); d < minDelay {
+		t.Fatalf("last delay was less than minimum. %s < %s", d, minDelay)
 	}
 
-	// Should get the same exact result for 3 or more simultaneous signals.
+	// Should get the same exact result as above (for 3 or more simultaneous signals
+	// 3rd, 4th, 5th should be skipped).
+	sentTime = time.Now()
 	go sendSignals(5, nil)
 	if n := countSignals(); n != 2 {
 		t.Fatalf("wrong signal count. expected reduction to 2, got %d", n)
 	}
-	if d := lastDelay(); d < minDelaySafe {
-		t.Fatalf("last delay was less than minimum. %s < %s", d, minDelaySafe)
+	if d := gotTimes[len(gotTimes)-1].Sub(sentTime); d < minDelay {
+		t.Fatalf("last delay was less than minimum. %s < %s", d, minDelay)
 	}
 
 	// Errors should go right through.
