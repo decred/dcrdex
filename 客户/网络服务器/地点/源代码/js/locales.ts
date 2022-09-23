0363b0c83080af3188f309ea15d6b8f562325d93
DominicTing <47611497+DominicTing@users.noreply.github.com>
2022-09-23 09:47:23 +0800

    Update locales.ts

--- 客户/网络服务器/地点/源代码/js/locales.ts
+++ 客户/网络服务器/地点/源代码/js/locales.ts
@@ -329,7 +329,7 @@ export const deDE: Locale = {
 const localesMap: Record<string, Locale> = {
   'en-us': enUS,
   'pt-br': ptBR,
-  'zh-cn': 简体中文 ,
+  'zh-cn': zhCN,
   'pl-pl': plPL,
   'de-de': deDE
 }
