636f1e266e4dff1652e880d449146ca472e0b0ab
JoeGruff <joegruffins@gmail.com>
2022-09-29 20:40:16 +0900

    [v0.5] server/db: Move cancel fields explicitly.

--- server/db/driver/pg/internal/orders.go
+++ server/db/driver/pg/internal/orders.go
@@ -242,6 +242,6 @@ const (
 		WHERE oid = $1
 		RETURNING oid, account_id, client_time, server_time, commit, target_order, %d, epoch_idx, epoch_dur, preimage
 	)
-	INSERT INTO %s
+	INSERT INTO %s (oid, account_id, client_time, server_time, commit, target_order, status, epoch_idx, epoch_dur, preimage)
 	SELECT * FROM moved;`
 )
