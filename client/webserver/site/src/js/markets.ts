09823ab5098fd4ceedae39ae5d15cfa322dad76b
norwnd <norwnd>
2022-11-12 21:33:21 +0300

    Fix typos in previous commit

--- client/webserver/site/src/js/markets.ts
+++ client/webserver/site/src/js/markets.ts
@@ -2196,7 +2196,7 @@ export default class MarketsPage extends BasePage {
     const rounded = String(lotsAdj) !== page.lotField.value
     const adjQty = lotsAdj * lotSize / bui.conventional.conversionFactor
 
-    return [false, rounded, lotsAdj, adjQty]
+    return [true, rounded, lotsAdj, adjQty]
   }
 
   qtyFieldKeyupHandler () {
@@ -2281,7 +2281,7 @@ export default class MarketsPage extends BasePage {
     const rounded = adjQtyAtom !== qtyRawAtom
     const adjQty = adjQtyAtom / bui.conventional.conversionFactor
 
-    return [false, rounded, adjLots, adjQty]
+    return [true, rounded, adjLots, adjQty]
   }
 
   /*
