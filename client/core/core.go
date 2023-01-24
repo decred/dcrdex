3c0365bfc6b392d17a0626a1cea9e004eef5597d
Jonathan Chappelow <chappjc@protonmail.com>
2023-01-24 16:25:55 -0600

    client/core: return from Run if db.Accounts fails in initialize

--- client/core/core.go
+++ client/core/core.go
@@ -1529,7 +1529,11 @@ func (c *Core) Run(ctx context.Context) {
 	// Store the context as a field, since we will need to spawn new DEX threads
 	// when new accounts are registered.
 	c.ctx = ctx
-	c.initialize() // connectDEX gets ctx for the wsConn
+	if err := c.initialize(); err != nil { // connectDEX gets ctx for the wsConn
+		c.log.Critical(err)
+		close(c.ready) // unblock <-Ready()
+		return
+	}
 	close(c.ready)
 
 	// The DB starts first and stops last.
@@ -6396,10 +6400,10 @@ func pluralize(n int) string {
 
 // initialize pulls the known DEXes from the database and attempts to connect
 // and retrieve the DEX configuration.
-func (c *Core) initialize() {
+func (c *Core) initialize() error {
 	accts, err := c.db.Accounts()
 	if err != nil {
-		c.log.Errorf("Error retrieving accounts from database: %v", err) // panic?
+		return fmt.Errorf("failed to retrieve accounts from database: %w", err)
 	}
 
 	// Start connecting to DEX servers.
@@ -6449,6 +6453,8 @@ func (c *Core) initialize() {
 				n, pluralize(n), host)
 		}
 	}
+
+	return nil
 }
 
 // connectAccount makes a connection to the DEX for the given account. If a
