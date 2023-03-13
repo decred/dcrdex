3a216ce3f750fa5e2f270a26d998d674024eb1f1
Jonathan Chappelow <chappjc@protonmail.com>
2023-03-13 10:01:57 -0500

    pkg: tray build in build script

--- pkg.sh
+++ pkg.sh
@@ -6,8 +6,6 @@ set -e
 VER="0.6.0-pre" # pre, beta, rc1, etc.
 META= # "release"
 
-TARGETS="linux/amd64 linux/arm64 windows/amd64 darwin/amd64 darwin/arm64"
-
 export CGO_ENABLED=0
 
 # if META set, append "+${META}", otherwise nothing.
@@ -22,28 +20,41 @@ npm run build
 popd
 
 rm -rf bin
-for TARGET in ${TARGETS}; do
-  OS=${TARGET%%/*}
-  ARCH=${TARGET##*/}
 
-  mkdir -p "bin/dexc-${OS}-${ARCH}-v${VER}"
+build_targets (){
+  for TARGET in ${TARGETS}; do
+    OS=${TARGET%%/*}
+    ARCH=${TARGET##*/}
+    echo "Building for ${OS}-${ARCH} with FLAVOR=${FLAVOR}"
 
-  pushd client/cmd/dexc
-  GOOS=${OS} GOARCH=${ARCH} go build -trimpath -o "../../../bin/dexc-${OS}-${ARCH}-v${VER}" -ldflags "$LDFLAGS"
-  popd
+    mkdir -p "bin/dexc${FLAVOR}-${OS}-${ARCH}-v${VER}"
 
-  pushd client/cmd/dexcctl
-  GOOS=${OS} GOARCH=${ARCH} go build -trimpath -o "../../../bin/dexc-${OS}-${ARCH}-v${VER}" -ldflags "$LDFLAGS"
-  popd
+    pushd client/cmd/dexc
+    GOOS=${OS} GOARCH=${ARCH} go build -trimpath -o "../../../bin/dexc${FLAVOR}-${OS}-${ARCH}-v${VER}" -ldflags "$LDFLAGS"
+    popd
 
-  pushd bin
-  if [[ "$OS" == "windows" ]]; then
-    zip -9 -r -q "dexc-${OS}-${ARCH}-v${VER}.zip" "dexc-${OS}-${ARCH}-v${VER}"
-  else
-    tar -I 'gzip -9' --owner=0 --group=0 -cf "dexc-${OS}-${ARCH}-v${VER}.tar.gz" "dexc-${OS}-${ARCH}-v${VER}"
-  fi
-  popd
-done
+    pushd client/cmd/dexcctl
+    GOOS=${OS} GOARCH=${ARCH} go build -trimpath -o "../../../bin/dexc${FLAVOR}-${OS}-${ARCH}-v${VER}" -ldflags "$LDFLAGS"
+    popd
+
+    pushd bin
+    if [[ "$OS" == "windows" ]]; then
+      zip -9 -r -q "dexc${FLAVOR}-${OS}-${ARCH}-v${VER}.zip" "dexc${FLAVOR}-${OS}-${ARCH}-v${VER}"
+    else
+      tar -I 'gzip -9' --owner=0 --group=0 -cf "dexc${FLAVOR}-${OS}-${ARCH}-v${VER}.tar.gz" "dexc${FLAVOR}-${OS}-${ARCH}-v${VER}"
+    fi
+    popd
+  done
+}
+
+# Vanilla builds on all supported os/arch targets
+TARGETS="linux/amd64 linux/arm64 windows/amd64 darwin/amd64 darwin/arm64"
+build_targets
+
+# Only windows gets the systray build
+TARGETS="windows/amd64"
+FLAVOR="-tray"
+build_targets
 
 echo "Files embedded in the Go webserver package:"
 go list -f '{{ .EmbedFiles }}' decred.org/dcrdex/client/webserver
