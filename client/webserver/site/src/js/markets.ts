0d455c5da79f6ba98a50ad5f35ffae77fd6279c2
norwnd <norwnd>
2023-04-05 07:37:10 +0300

    Remove redundant validation

--- client/webserver/site/src/js/markets.ts
+++ client/webserver/site/src/js/markets.ts
@@ -2587,8 +2587,6 @@ class MarketList {
   updateSpots (note: SpotPriceNote) {
     for (const row of this.markets) {
       if (row.mkt.xc.host !== note.host) continue
-      const spot = note.spots[row.mkt.name]
-      if (!spot) continue
       const xc = app().exchanges[row.mkt.xc.host]
       const mkt = xc.markets[row.mkt.name]
       setPriceAndChange(row.tmpl, xc, mkt)
