d892fde4b2b4686d49bda422041c0b450688c6f0
Jonathan Chappelow <chappjc@protonmail.com>
2023-05-05 09:09:28 -0500

    server/asset/{btc,zec}: tx hasher and deserializer needed

--- server/asset/zec/zec.go
+++ server/asset/zec/zec.go
@@ -107,6 +107,10 @@ func NewBackend(configPath string, logger dex.Logger, network dex.Network) (asse
 			}
 			return zecTx.MsgTx, nil
 		},
+		TxHasher: func(tx *wire.MsgTx) *chainhash.Hash {
+			h := zecTx(tx).TxHash()
+			return &h
+		},
 		DumbFeeEstimates:     true,
 		FeeConfs:             feeConfs,
 		ManualMedianFee:      true,
@@ -266,4 +270,8 @@ func shieldedIO(tx *btc.VerboseTxExtended) (in, out uint64, err error) {
 		return 0, 0, err
 	}
 	return feeTx.shieldedIn, feeTx.shieldedOut, nil
+}
+
+func zecTx(tx *wire.MsgTx) *dexzec.Tx {
+	return dexzec.NewTxFromMsgTx(tx, dexzec.MaxExpiryHeight)
 }
