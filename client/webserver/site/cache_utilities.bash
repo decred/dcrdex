c518f3bc44f09a2349becb86d1d55940e18f603c
Brian Stafford <buck54321@gmail.com>
2023-06-05 18:33:22 -0500

    hash iterating ls-files

--- client/webserver/site/cache_utilities.bash
+++ client/webserver/site/cache_utilities.bash
@@ -4,7 +4,17 @@ CSS_DIR=${SITE_DIR}/src/css
 JS_DIR=${SITE_DIR}/src/js
 
 bbhash () { sha1sum ${SITE_DIR}/src/html/bodybuilder.tmpl | head -n1 | cut -d " " -f1 ; }
-hashdir () { sha1sum $1/* | md5sum | head -n1 | cut -d " " -f1 ; }
+hashfile () { sha1sum $1 | cut -d " " -f1 ; }
+hashdir () {
+    cd ${SITE_DIR}/src/css
+    HASHBUF=""
+    while read FP ; do
+        HASHBUF="${HASHBUF}`hashfile ${FP}`"
+    done <<< `git ls-files .`
+    echo ${HASHBUF} | sha1sum | cut -d " " -f1 | cut -c1-12
+    cd - > /dev/null
+}
+
 setcssbuster () {
     sed -i.tmp "s/href=\"\/css\/style.css?v=\([^\"]*\)\"/href=\"\/css\/style.css?v=`hashdir ${CSS_DIR}`\"/" ${BB_FILEPATH}
     rm ${BB_FILEPATH}.tmp
