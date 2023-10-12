a1b660daf267ad1ba117ec0879cc89e8dc86a51b
Philemon Ukane <ukanephilemon@gmail.com>
2023-10-12 20:17:28 +0100

    fix bug on add server page
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>

--- client/webserver/site/src/js/forms.ts
+++ client/webserver/site/src/js/forms.ts
@@ -1607,7 +1607,7 @@ export class DEXAddressForm {
       }
       return
     }
-    if (!this.dexToUpdate && (skipRegistration || res.paid || Object.keys(res.xc.pendingBonds).length > 0)) {
+    if (!this.dexToUpdate && (skipRegistration || res.paid)) {
       await app().fetchUser()
       await app().loadPage('markets')
       return
