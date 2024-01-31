945e48ab79f40308dd5759c7ac9c04d9aef9e2d4
Philemon Ukane <ukanephilemon@gmail.com>
2024-01-31 16:29:26 +0100

    Fix bond wallet panic in core.initializeDEXConnections (#2672)
    * Fix bond wallet panic in core.initializeDEXConnections
    ---------
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>

--- client/core/core.go
+++ client/core/core.go
@@ -5231,10 +5231,12 @@ func (c *Core) initializeDEXConnections(crypter encrypt.Crypter) {
 				unbip(bondAssetID), targetTier, dc.acct.host, maxBondedAmt)
 			wallet, exists := c.wallet(bondAssetID)
 			if !exists || !wallet.connected() { // connectWallets already run, just fail
-				subject, _ := c.formatDetails(TopicWalletConnectionWarning)
-				c.notify(newWalletConfigNote(TopicWalletConnectionWarning, subject,
-					fmt.Sprintf("bond asset wallet %s not configured or connected", unbip(bondAssetID)),
-					db.ErrorLevel, wallet.state()))
+				subject, details := c.formatDetails(TopicBondWalletNotConnected, unbip(bondAssetID))
+				var w *WalletState
+				if exists {
+					w = wallet.state()
+				}
+				c.notify(newWalletConfigNote(TopicBondWalletNotConnected, subject, details, db.ErrorLevel, w))
 			} else if !wallet.unlocked() {
 				err = wallet.Unlock(crypter)
 				if err != nil {
