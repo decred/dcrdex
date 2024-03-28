72b2fa912653f914bd20aaf56d29c1a91c561795
Marton <marci93@gmail.com>
2024-03-28 14:08:55 -0400

    client/asset/{eth,btc}: Quiet transaction db logs (#2718)
    Lowers severity of Info and Debug logs by one level.

--- client/asset/btc/txdb.go
+++ client/asset/btc/txdb.go
@@ -73,12 +73,23 @@ type BadgerTxDB struct {
 }
 
 // badgerLoggerWrapper wraps dex.Logger and translates Warnf to Warningf to
-// satisfy badger.Logger.
+// satisfy badger.Logger. It also lowers the log level of Infof to Debugf
+// and Debugf to Tracef.
 type badgerLoggerWrapper struct {
 	dex.Logger
 }
 
 var _ badger.Logger = (*badgerLoggerWrapper)(nil)
+
+// Debugf -> dex.Logger.Tracef
+func (log *badgerLoggerWrapper) Debugf(s string, a ...interface{}) {
+	log.Tracef(s, a...)
+}
+
+// Infof -> dex.Logger.Debugf
+func (log *badgerLoggerWrapper) Infof(s string, a ...interface{}) {
+	log.Debugf(s, a...)
+}
 
 // Warningf -> dex.Logger.Warnf
 func (log *badgerLoggerWrapper) Warningf(s string, a ...interface{}) {
