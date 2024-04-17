89d9f60c0847a8651d9061eacd6344a17ca076ea
Peter Banik <peter@froggle.org>
2024-04-17 16:10:41 +0200

    Translated strings

--- client/webserver/locales/en-us.go
+++ client/webserver/locales/en-us.go
@@ -167,6 +167,8 @@ var EnUS = map[string]*intl.Translation{
 	"Save Notifications":        {T: "Save Notifications"},
 	"Companion App":             {T: "Companion App"},
 	"companion_app_info":        {T: "Install the DCRDEX companion app and scan this QR code."},
+	"companion_app_tor_needed":  {T: "Tor must be enabled for this."},
+	"Pair companion app":        {T: "Pair companion app"},
 	"Download APK":              {T: "Download APK"},
 	"Build ID":                  {T: "Build ID"},
 	"Connect":                   {T: "Connect"},
