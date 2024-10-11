b61ca27a97d3056464b7439bf55e780e4b4b42ee
Brian Stafford <buck54321@gmail.com>
2024-10-10 19:24:26 -0500

    update binance book stream url

--- client/core/core_test.go
+++ client/core/core_test.go
@@ -346,6 +346,8 @@ func (conn *TWebsocket) Connect(context.Context) (*sync.WaitGroup, error) {
 	return &sync.WaitGroup{}, conn.connectErr
 }
 
+func (conn *TWebsocket) UpdateURL(string) {}
+
 type TDB struct {
 	updateWalletErr          error
 	acct                     *db.AccountInfo
