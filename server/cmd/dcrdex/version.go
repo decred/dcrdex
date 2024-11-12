f1d74a515f249b4ca53e1b8c0c7c2bf79ac2d6f9
Brian Stafford <buck54321@gmail.com>
2024-11-12 08:26:14 -0600

    bump version numbers to 1.0.2

--- server/cmd/dcrdex/version.go
+++ server/cmd/dcrdex/version.go
@@ -39,7 +39,7 @@ var (
 	// and build metadata portions MUST only contain characters from
 	// semanticAlphabet.
 	// NOTE: The Version string is overridden on init.
-	Version = "1.0.1"
+	Version = "1.0.2+release.local"
 )
 
 func init() {
