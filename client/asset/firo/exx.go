ebbcb9cdc0d57db54267062ee7b516381107b0c3
dev-warrior777 <>
2024-12-12 15:54:28 +0800

    imports: update go.mod for btcsuite/btcd/btcutil/base58  #5

--- client/asset/firo/exx.go
+++ client/asset/firo/exx.go
@@ -5,9 +5,9 @@ import (
 	"strings"
 
 	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/btcutil/base58"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/txscript"
-	"github.com/btcsuite/btcutil/base58"
 )
 
 const (
