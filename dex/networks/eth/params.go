af5c27e80a90e2b52ab6234bba61133da33a7c85
JoeGruff <joegruffins@gmail.com>
2024-12-17 16:29:44 +0900

    harness: Update eth to work with geth 1.14 and up.

--- dex/networks/eth/params.go
+++ dex/networks/eth/params.go
@@ -32,8 +32,8 @@ const (
 // These are the chain IDs of the various Ethereum network supported.
 const (
 	MainnetChainID = 1
-	TestnetChainID = 5  // Görli
-	SimnetChainID  = 42 // see dex/testing/eth/harness.sh
+	TestnetChainID = 5    // Görli
+	SimnetChainID  = 1337 // see dex/testing/eth/harness.sh
 )
 
 var (
