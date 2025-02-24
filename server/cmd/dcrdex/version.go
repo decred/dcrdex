8e608ffc3167057f4e4a95787d589c59c58a3a73
martonp <marci93@gmail.com>
2025-02-24 09:10:01 -0500

    bump version numbers to 1.0.3

--- server/cmd/dcrdex/version.go
+++ server/cmd/dcrdex/version.go
@@ -39,7 +39,7 @@ var (
 	// and build metadata portions MUST only contain characters from
 	// semanticAlphabet.
 	// NOTE: The Version string is overridden on init.
-	Version = "1.0.2+release.local"
+	Version = "1.0.3+release.local"
 )
 
 func init() {
