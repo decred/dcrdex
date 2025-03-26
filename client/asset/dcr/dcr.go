1f1ebff0d921a8913b91d928be14030a8170c68a
Philemon Ukane <ukanephilemon@gmail.com>
2025-03-26 11:39:37 +0100

    multi: Add ` ExchangeWalletCustom` to support external btc-clone wallets (#2855)
    * add ExchangeWalletCustom
    
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>
    
    * client/asset/btc: expose spv scanFilters method
    
    * export useful methods
    
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>
    
    * fix comments and remove redundant method
    
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>
    
    * revert AccountInfo changes in https://github.com/decred/dcrdex/pull/2636/commits/d7a54886f062d0916f1f4da64c85f9470246b0bf
    
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>
    
    * export dcr wallet options
    
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>
    
    ---------
    
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>
    Co-authored-by: Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>

--- client/asset/dcr/dcr.go
+++ client/asset/dcr/dcr.go
@@ -126,7 +126,7 @@ var (
 	// be tried over and over with wallet in SPV mode.
 	maxRedeemMempoolAge = time.Hour * 2
 
-	walletOpts = []*asset.ConfigOption{
+	WalletOpts = []*asset.ConfigOption{
 		{
 			Key:         "fallbackfee",
 			DisplayName: "Fallback fee rate",
@@ -276,7 +276,7 @@ var (
 				Type:             walletTypeSPV,
 				Tab:              "Native",
 				Description:      "Use the built-in SPV wallet",
-				ConfigOpts:       walletOpts,
+				ConfigOpts:       WalletOpts,
 				Seeded:           true,
 				MultiFundingOpts: multiFundingOpts,
 			},
@@ -285,7 +285,7 @@ var (
 				Tab:               "External",
 				Description:       "Connect to dcrwallet",
 				DefaultConfigPath: defaultConfigPath,
-				ConfigOpts:        append(rpcOpts, walletOpts...),
+				ConfigOpts:        append(rpcOpts, WalletOpts...),
 				MultiFundingOpts:  multiFundingOpts,
 			},
 		},
