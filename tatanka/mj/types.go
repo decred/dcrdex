76055fa5fa0ce545cf983078ebbc5503be9de1f3
martonp <marci93@gmail.com>
2025-04-02 11:29:44 -0400

    tatanka: Improve client connection maintenance
    Starts some groundwork on making the client connection to the mesh more
    robust.
    
    - Adds an http request to the tatanka node that allows clients to fetch
      its whitelist.
    - Client connection maintains two connections to the mesh, one primary
      and one secondary.
    - The primary connection is used for sending messages and subscribing
      to topics. The secondary connection is maintained for quick failover.
    - Updates the client connect request to include a set of initial
      subscriptions.
    - Adds a UpdateSubscriptions request to update all the subscriptions of a
      client in a single request. This is useful when upgrading the secondary
      connection to the primary connection.
    - The `conn.MessageHandlers` tatanka request and peer request handlers are
      updated to include a respond function to avoid having to look up the node
      by ID to respond to a request.

--- tatanka/mj/types.go
+++ tatanka/mj/types.go
@@ -25,6 +25,7 @@ const (
 	ErrBanned
 	ErrFailedRelay
 	ErrUnknownSender
+	ErrCapacity
 )
 
 const (
@@ -39,19 +40,22 @@ const (
 	RouteShareScore       = "share_score"
 
 	// tatanka <=> client
-	RouteConnect     = "connect"
-	RouteConfig      = "config"
-	RoutePostBond    = "post_bond"
-	RouteSubscribe   = "subscribe"
-	RouteUnsubscribe = "unsubscribe"
-	RouteRates       = "rates"
-	RouteSetScore    = "set_score"
+	RouteConnect             = "connect"
+	RoutePostBond            = "post_bond"
+	RouteSubscribe           = "subscribe"
+	RouteUnsubscribe         = "unsubscribe"
+	RouteUpdateSubscriptions = "update_subscriptions"
+	RouteRates               = "rates"
+	RouteSetScore            = "set_score"
 
 	// client1 <=> tatankanode <=> client2
 	RouteTankagram     = "tankagram"
 	RouteEncryptionKey = "encryption_key"
 	RouteBroadcast     = "broadcast"
 	RouteNewSubscriber = "new_subscriber"
+
+	// HTTP Requests, used before client established WS connection
+	RouteNodeInfo = "node_info"
 )
 
 const (
@@ -86,7 +90,12 @@ type TatankaConfig struct {
 }
 
 type Connect struct {
-	ID tanka.PeerID `json:"id"`
+	ID          tanka.PeerID                    `json:"id"`
+	InitialSubs map[tanka.Topic][]tanka.Subject `json:"initialSubs"`
+}
+
+type UpdateSubscriptions struct {
+	Subscriptions map[tanka.Topic][]tanka.Subject `json:"subscriptions"`
 }
 
 type Disconnect = Connect
@@ -142,6 +151,8 @@ type TankagramResult struct {
 }
 
 type Broadcast struct {
+	// TOOD: Why is PeerID part of the broadcast message when it can be
+	// determined by the request?
 	PeerID      tanka.PeerID         `json:"peerID"`
 	Topic       tanka.Topic          `json:"topic"`
 	Subject     tanka.Subject        `json:"subject"`
