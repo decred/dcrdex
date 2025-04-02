36ce1b55fcd22c8e6403719bb3ea687256efc26a
JoeGruffins <34998433+JoeGruffins@users.noreply.github.com>
2025-04-02 03:44:04 +0000

    tatanka: Update orderbook for push data. (#3221)

--- tatanka/tanka/swaps.go
+++ tatanka/tanka/swaps.go
@@ -13,6 +13,8 @@ import (
 	"github.com/decred/dcrd/crypto/blake256"
 )
 
+const orderIDLen = 40
+
 type Order struct {
 	From    PeerID `json:"from"`
 	BaseID  uint32 `json:"baseID"`
@@ -21,32 +23,20 @@ type Order struct {
 	Qty     uint64 `json:"qty"`
 	Rate    uint64 `json:"rate"`
 	// LotSize: Tatankanet does not prescribe a lot size. Instead, users must
-	// select their own minimum minimum lot size. The user's UI should ignore
+	// select their own minimum lot size. The user's UI should ignore
 	// orderbook orders that don't have the requisite lot size. The UI should
 	// show lot size selection in terms of a sliding scale of fee exposure.
 	// Lot sizes can only be powers of 2.
 	LotSize uint64    `json:"lotSize"`
+	Nonce   uint64    `json:"nonce"`
 	Stamp   time.Time `json:"stamp"`
-	// Nonce can be used to force unique ids while other values are the same.
-	Nonce uint32 `json:"nonce"`
 }
 
-func (ord *Order) ID() [32]byte {
-	const msgLen = 32 + 4 + 4 + 1 + 8 + 8 + 8 + 8 + 4
-	b := make([]byte, msgLen)
+func (ord *Order) ID() ID40 {
+	var b ID40
 	copy(b[:32], ord.From[:])
-	binary.BigEndian.PutUint32(b[32:36], ord.BaseID)
-	binary.BigEndian.PutUint32(b[36:40], ord.QuoteID)
-	if ord.Sell {
-		b[41] = 1
-	}
-	binary.BigEndian.PutUint64(b[41:49], ord.Qty)
-	binary.BigEndian.PutUint64(b[49:57], ord.Rate)
-	binary.BigEndian.PutUint64(b[57:65], ord.LotSize)
-	binary.BigEndian.PutUint64(b[65:73], uint64(ord.Stamp.UnixMilli()))
-	binary.BigEndian.PutUint32(b[73:77], ord.Nonce)
-	return blake256.Sum256(b)
-
+	binary.BigEndian.PutUint64(b[32:], ord.Nonce)
+	return b
 }
 
 func (ord *Order) Valid() error {
@@ -75,9 +65,15 @@ func (i ID32) String() string {
 	return hex.EncodeToString(i[:])
 }
 
+type ID40 [40]byte
+
+func (i ID40) String() string {
+	return hex.EncodeToString(i[:])
+}
+
 type Match struct {
 	From    PeerID    `json:"from"`
-	OrderID ID32      `json:"orderID"`
+	OrderID ID40      `json:"orderID"`
 	Qty     uint64    `json:"qty"`
 	Stamp   time.Time `json:"stamp"`
 }
@@ -93,7 +89,7 @@ func (m *Match) ID() ID32 {
 }
 
 type MatchAcceptance struct {
-	OrderID ID32 `json:"orderID"`
+	OrderID ID40 `json:"orderID"`
 	MatchID ID32 `json:"matchID"`
 }
 
@@ -102,48 +98,16 @@ type MarketParameters struct {
 	QuoteID uint32 `json:"quoteID"`
 }
 
-const (
-	OrderExpiration    = time.Hour * 12
-	OrderUpdateVersion = 0
-)
-
 type OrderUpdate struct {
-	*Order
-	Version    uint8     `json:"version"`
-	Expiration time.Time `json:"expiration"`
-	Settled    uint64    `json:"settled"`
-	// The signature of all other serialized fields with private key
-	// belonging to PeerID.
-	Sig []byte `json:"sig"`
+	From  PeerID    `json:"from"`
+	Nonce uint64    `json:"nonce"`
+	Qty   uint64    `json:"qty"`
+	Stamp time.Time `json:"stamp"`
 }
 
-func NewOrderUpdate(o *Order, settled uint64) *OrderUpdate {
-	return &OrderUpdate{
-		Version:    OrderUpdateVersion,
-		Expiration: o.Stamp.Add(OrderExpiration),
-		Settled:    settled,
-		Order:      o,
-	}
-}
-
-// Serialize returns the bytes needed to sign the update.
-func (ou *OrderUpdate) Serialize() ([]byte, error) {
-	msgLen := 1 + 32 + 4 + 4 + 1 + 8 + 8 + 8 + 8 + 4 + 8 + 8
-	b := make([]byte, msgLen)
-	b[0] = ou.Version
-	copy(b[1:33], ou.From[:])
-	binary.BigEndian.PutUint32(b[33:37], ou.BaseID)
-	binary.BigEndian.PutUint32(b[37:41], ou.QuoteID)
-	if ou.Sell {
-		b[42] = 1
-	}
-	binary.BigEndian.PutUint64(b[42:50], ou.Qty)
-	binary.BigEndian.PutUint64(b[50:58], ou.Rate)
-	binary.BigEndian.PutUint64(b[58:66], ou.LotSize)
-	binary.BigEndian.PutUint64(b[66:74], uint64(ou.Stamp.UnixMilli()))
-	binary.BigEndian.PutUint32(b[74:78], ou.Nonce)
-	binary.BigEndian.PutUint64(b[78:84], uint64(ou.Expiration.UnixMilli()))
-	binary.BigEndian.PutUint64(b[84:92], ou.Settled)
-
-	return b, nil
+func (ou *OrderUpdate) ID() ID40 {
+	var b ID40
+	copy(b[:32], ou.From[:])
+	binary.BigEndian.PutUint64(b[32:], ou.Nonce)
+	return b
 }
