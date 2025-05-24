6e95c2bbfd24cf3995bae38b11d06d193d8abac3
Philemon Ukane <ukanephilemon@gmail.com>
2025-05-24 14:11:13 +0100

    multi: bump app version(s)
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>

--- pkg.sh
+++ pkg.sh
@@ -3,7 +3,7 @@
 set -e
 
 # For release, remove pre-release info, and set metadata to "release".
-VER="1.0.1-pre" # pre, beta, rc1, etc.
+VER="1.0.4-pre" # pre, beta, rc1, etc.
 META= # "release"
 
 export CGO_ENABLED=0
