cde38cce5f45da4a4fe478fcc639aa6ce6caae95
JoeGruff <joegruffins@gmail.com>
2025-10-06 17:39:16 +0900

    web: Fix error enums and update deps.

--- client/webserver/site/src/js/http.ts
+++ client/webserver/site/src/js/http.ts
@@ -38,6 +38,7 @@ export async function getJSON (addr: string): Promise<any> {
 export enum Errors {
   walletErr,
   walletAuthErr,
+  noAuthErr,
   walletBalanceErr,
   dupeDEXErr,
   assetSupportErr,
