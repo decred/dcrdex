a46d8d5b65f9dbe3d1ed12d243589d5daec814db
Justin Do <justin.namdo@gmail.com>
2025-10-23 15:32:12 +0700

    Fix digibyte simnet harness (#3391)
    * fix dgb dimnet harness
    
    * remove comment not need

--- dex/testing/dgb/harness.sh
+++ dex/testing/dgb/harness.sh
@@ -18,19 +18,24 @@ export GAMMA_ADDRESS="dgbrt1q46scevzm9hu06y0pnl8v2fxmll9v8k3e3lnd43"
 # Delta is a named wallet in the beta wallet directory.
 export DELTA_WALLET_SEED="ehA5EC6mbNvGCJ4HqtjNc822ojnU2bGRLoc3cZipoYFFYT7tYrq4"
 export DELTA_ADDRESS="dgbrt1qdgzj8guegzuyfupcvy3vjlpfxpv8rgruqtkndf"
-# Signal that the node needs to restart after encrypting wallet
-export RESTART_AFTER_ENCRYPT="1"
+# Creates named wallets as legacy (BDB) format. DigiByte v8.26.0 defaults to
+# descriptor wallets, but sethdseed only works with legacy wallets.
 # $1 is the node to create with. $2 is the wallet name
-export NEW_WALLET_CMD="./\$1 createwallet \$2"
+export NEW_WALLET_CMD="./\$1 -named createwallet wallet_name=\$2 descriptors=false"
 export BLOCKS_TO_MINE=120 # it chokes with high diff if you mine much more, but if you stop at 100, nothing much is mature it seems
-export EXTRA_ARGS="-disabledandelion=1 -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -nodiscover -nodnsseed" # -onlynet=ipv4 -nodiscover
+# DigiByte v8.22+ (based on Bitcoin Core v0.21+) does not create default wallet automatically
+export CREATE_DEFAULT_WALLET="1"
+# -deprecatedrpc=create_bdb: Re-enable BDB wallet creation (deprecated in v8.26.0)
+# Removed -disabledandelion=1 (invalid in v8.22+, correct syntax is -dandelion=0)
+export EXTRA_ARGS="-deprecatedrpc=create_bdb -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0 -nodiscover -nodnsseed"
 
 # Background watch mining by default:
-# 'export NOMINER="1"' or uncomment this line to disable
-#
+# Set 'export NOMINER="1"' to disable the miner window.
+# Must use 'export' keyword to pass variable to base-harness.sh child process.
+
 # TODO: Cannot presently continuously mine on simnet but when we can comment
 # this out.
-NOMINER="1"
+# export NOMINER="1"
 
 # Run the harness
 ../btc/base-harness.sh
