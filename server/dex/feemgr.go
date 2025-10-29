7005d601b008b2a507aa8282b109e33c219039ff
buck54321 <buck54321@gmail.com>
2025-10-29 09:42:34 -0500

    server/dex: stash old fee rate momentarily after rate increase to avoid races (#3397)
    * stash old rate momentarily after rate increase to avoid races
    
    * simplify design

--- server/dex/feemgr.go
+++ server/dex/feemgr.go
@@ -2,7 +2,7 @@ package dex
 
 import (
 	"context"
-	"strconv"
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -10,10 +10,9 @@ import (
 	"decred.org/dcrdex/server/market"
 )
 
-// FeeManager manages fee fetchers and a fee cache.
+// FeeManager manages FeeFetchers.
 type FeeManager struct {
-	assets map[uint32]*asset.BackedAsset
-	cache  map[uint32]*uint64
+	fetchers map[uint32]*feeFetcher
 }
 
 var _ market.FeeSource = (*FeeManager)(nil)
@@ -21,62 +20,61 @@ var _ market.FeeSource = (*FeeManager)(nil)
 // NewFeeManager is the constructor for a FeeManager.
 func NewFeeManager() *FeeManager {
 	return &FeeManager{
-		assets: make(map[uint32]*asset.BackedAsset),
-		cache:  make(map[uint32]*uint64),
+		fetchers: make(map[uint32]*feeFetcher),
 	}
 }
 
-// AddFetcher adds a fee fetcher (a *BackedAsset) and primes the cache. The
-// asset's MaxFeeRate are used to limit the rates returned by the LastRate
-// method as well as the rates returned by child FeeFetchers.
+// AddFetcher adds a FeeFetcher for an asset.
 func (m *FeeManager) AddFetcher(asset *asset.BackedAsset) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	rate, err := asset.Backend.FeeRate(ctx)
-	if err != nil {
-		log.Warnf("Error priming fee cache for %s: %v", asset.Symbol, err)
-	}
-	if rate > asset.MaxFeeRate {
-		rate = asset.MaxFeeRate
-	}
-	m.cache[asset.ID] = &rate
-	m.assets[asset.ID] = asset
+	m.fetchers[asset.ID] = newFeeFetcher(asset)
 }
 
-// FeeFetcher creates and returns an asset-specific fetcher that satisfies
-// market.FeeFetcher, implemented by *feeFetcher.
+// FeeFetcher returns the specified asset's FeeFetcher.
 func (m *FeeManager) FeeFetcher(assetID uint32) market.FeeFetcher {
-	asset := m.assets[assetID]
-	if asset == nil {
-		panic("no fetcher for " + strconv.Itoa(int(assetID)))
-	}
-	return newFeeFetcher(asset, m.cache[assetID])
+	return m.fetchers[assetID]
 }
 
 // LastRate is the last rate cached for the specified asset.
 func (m *FeeManager) LastRate(assetID uint32) uint64 {
-	r := m.cache[assetID]
-	if r == nil {
-		return 0
-	}
-	return atomic.LoadUint64(r)
+	return m.fetchers[assetID].LastRate()
 }
 
 // feeFetcher implements market.FeeFetcher and updates the last fee rate cache.
 type feeFetcher struct {
 	*asset.BackedAsset
 	lastRate *uint64
+
+	// Stash the old rate for a short time to avoid a race condition where
+	// a client gets a rate right before the server gets a higher rate, then the
+	// client tries to use the old rate and is rejected.
+	stashedRate struct {
+		sync.RWMutex
+		rate  uint64
+		stamp time.Time
+	}
 }
 
 var _ market.FeeFetcher = (*feeFetcher)(nil)
 
 // newFeeFetcher is the constructor for a *feeFetcher.
-func newFeeFetcher(asset *asset.BackedAsset, lastRate *uint64) *feeFetcher {
+func newFeeFetcher(asset *asset.BackedAsset) *feeFetcher {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	r, err := asset.Backend.FeeRate(ctx)
+	if err != nil {
+		log.Warnf("Error priming fee cache for %s: %v", asset.Symbol, err)
+	}
+	if r > asset.MaxFeeRate {
+		r = asset.MaxFeeRate
+	}
 	return &feeFetcher{
 		BackedAsset: asset,
-		lastRate:    lastRate,
+		lastRate:    &r,
 	}
 }
+
+// Use the lower rate for a minute after a rate increase to avoid races.
+const stashedRateExpiry = time.Minute
 
 // FeeRate fetches a new fee rate and updates the cache.
 func (f *feeFetcher) FeeRate(ctx context.Context) uint64 {
@@ -91,14 +89,32 @@ func (f *feeFetcher) FeeRate(ctx context.Context) uint64 {
 	if r > f.Asset.MaxFeeRate {
 		r = f.Asset.MaxFeeRate
 	}
-	atomic.StoreUint64(f.lastRate, r)
+	oldRate := atomic.SwapUint64(f.lastRate, r)
+	if oldRate < r {
+		f.stashedRate.Lock()
+		f.stashedRate.rate = oldRate
+		f.stashedRate.stamp = time.Now()
+		f.stashedRate.Unlock()
+		return oldRate
+	}
+	f.stashedRate.RLock()
+	if time.Since(f.stashedRate.stamp) < stashedRateExpiry && f.stashedRate.rate < r {
+		r = f.stashedRate.rate
+	}
+	f.stashedRate.RUnlock()
 	return r
 }
 
 // LastRate is the last rate cached. This may be used as a fallback if FeeRate
 // times out, or as a quick rate when rate freshness is not critical.
 func (f *feeFetcher) LastRate() uint64 {
-	return atomic.LoadUint64(f.lastRate)
+	r := atomic.LoadUint64(f.lastRate)
+	f.stashedRate.RLock()
+	if time.Since(f.stashedRate.stamp) < stashedRateExpiry && f.stashedRate.rate < r {
+		r = f.stashedRate.rate
+	}
+	f.stashedRate.RUnlock()
+	return r
 }
 
 // MaxFeeRate is a getter for the BackedAsset's dex.Asset.MaxFeeRate. This is
