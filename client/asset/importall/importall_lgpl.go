4fe39337aaaf7475054ff49fe2d5cfcb8bace191
Brian Stafford <buck54321@gmail.com>
2025-12-02 09:41:47 -0600

    disable base

--- client/asset/importall/importall_lgpl.go
+++ client/asset/importall/importall_lgpl.go
@@ -1,7 +1,8 @@
 package importall
 
 import (
-	_ "decred.org/dcrdex/client/asset/base"    // register base network
+	// Base is disable until we get L1 security fees worked out.
+	// _ "decred.org/dcrdex/client/asset/base"    // register base network
 	_ "decred.org/dcrdex/client/asset/eth"     // register eth asset
 	_ "decred.org/dcrdex/client/asset/polygon" // register polygon network
 )
