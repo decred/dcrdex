c42288fb13ae5c09b6c02e9e7e0bf0c97432d218
JoeGruff <joegruffins@gmail.com>
2025-12-03 15:07:22 +0900

    multi: Prepare v1.0.5.

--- client/cmd/bwctl/version.go
+++ client/cmd/bwctl/version.go
@@ -41,7 +41,7 @@ var (
 	// and build metadata portions MUST only contain characters from
 	// semanticAlphabet.
 	// NOTE: The Version string is overridden on init.
-	Version = "1.0.4+release.local"
+	Version = "1.0.5+release.local"
 )
 
 func init() {
