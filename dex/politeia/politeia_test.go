1b3ee13c998c940a8e71c4893ec0bf38f3ab2f4e
Philemon Ukane <ukanephilemon@gmail.com>
2026-01-05 02:35:30 +0100

    client/core: Implement politeia methods in core
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>

--- dex/politeia/politeia_test.go
+++ dex/politeia/politeia_test.go
@@ -1,7 +1,7 @@
 // This code is available on the terms of the project LICENSE.md file,
 // also available online at https://blueoakcouncil.org/license/1.0.0.
 
-///go:build live
+//go:build live
 
 package pi
 
