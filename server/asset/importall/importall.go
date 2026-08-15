178dde91503e4d9e41318cbf6d667fae6f08a94a
realrouse <real0rouse@gmail.com>
2026-08-16 01:32:52 +0300

    server/asset/lbc: add LBRY Credits backend
    lbcd node RPC clone using the LBC header decoder.

--- server/asset/importall/importall.go
+++ server/asset/importall/importall.go
@@ -8,6 +8,7 @@ import (
 	_ "decred.org/dcrdex/server/asset/dgb"  // register dgb asset
 	_ "decred.org/dcrdex/server/asset/doge" // register doge asset
 	_ "decred.org/dcrdex/server/asset/firo" // register firo asset
+	_ "decred.org/dcrdex/server/asset/lbc"  // register lbc asset
 	_ "decred.org/dcrdex/server/asset/ltc"  // register ltc asset
 	_ "decred.org/dcrdex/server/asset/xmr"  // register xmr asset
 	_ "decred.org/dcrdex/server/asset/zec"  // register zec asset
